4f32c68bf2b40e88fa89240eacb4c7c7e03183ba
Nick P <nb.piscitello@gmail.com>
2015-09-09 17:05:36 -0400

    completed challenge 9; ignoring the .png file generated

---
+++ ch9.py
@@ -0,0 +1,49 @@
+import re
+from PIL import Image,ImageDraw
+
+path = 'ch9_source'
+raw = open(path, 'r').read()
+
+searchObj = re.search(r'first:\s(.+)\s\ssecond:\s(.+)\s\s-->', raw, flags=re.DOTALL)
+
+raw = [None, None]
+raw[0] = searchObj.group(1)
+raw[1] = searchObj.group(2)
+
+#print 'first: %s' % raw[0]
+#print 'second: %s' % raw[1]
+
+# do for loop: add to current index until comma; comma increments
+data_list = [[],[]]
+for i in range(0,len(data_list)):
+	x = 0
+	for j in raw[i]:
+		if j == ',':
+			x += 1
+		elif j == '\n':
+			pass
+		else:
+			try:
+				data_list[i][x] += j
+			except IndexError:
+				data_list[i].append(j)
+
+for i in range(0,len(data_list)):
+	for j in range (0,len(data_list[i])):
+		data_list[i][j] = int(data_list[i][j])
+
+#print '\nfirst: %s' % data_list[0],
+#print '          length: %d' % len(data_list[0])
+#print '\nsecond: %s' % data_list[1],
+#print '          length: %d' % len(data_list[1])
+
+if max(data_list[0]) > max(data_list[1]):
+	size = max(data_list[0])
+else:
+	size = max(data_list[1])
+
+im = Image.new('1', (size,size), 1)
+draw = ImageDraw.Draw(im)
+draw.line(data_list[0])
+draw.line(data_list[1])
+im.save('first+second.png')
