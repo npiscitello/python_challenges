2a19c6d709e2b9dab59e8ea45060922255ad6c18
NP <nb.piscitello@gmail.com>
2017-10-23 13:22:21 -0400

    auth working, need to tune regex

--- ch09.py
+++ ch09.py
@@ -1,10 +1,14 @@
 #!/bin/python
 
 import urllib.request
+import codecs
 import re
 
 url = "http://www.pythonchallenge.com/pc/return/good.html";
-source = urllib.request.urlopen(url).read();
-first = re.search(r"first:\n(.*)\n\n", str(source))[1];
+auth = codecs.encode(b"huge:file", "base64").rstrip().decode("utf-8");
+req = urllib.request.Request(url, None, {"Authorization": "Basic " + auth})
+print(req.headers);
+source = urllib.request.urlopen(req).read();
+first = re.search(r"first:\n(.*)\n", str(source))[1];
 
 print(first);
